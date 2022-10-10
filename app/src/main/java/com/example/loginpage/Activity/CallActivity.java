b762dbda0b7aee2fd7a8cb3fffed3276bd21b66f
rites <RitheshaShettigar>
2022-10-10 10:18:08 +0530

    commit

---
+++ app/src/main/java/com/example/loginpage/Activity/CallActivity.java
@@ -0,0 +1,16 @@
+package com.example.loginpage.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.loginpage.R;
+
+public class CallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+    }
+}
